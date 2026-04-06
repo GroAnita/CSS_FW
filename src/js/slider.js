65110dcd46bd50dbe72c7758bed3d0cfe04cbad2
GroAnita <gro.anita.brathen@gmail.com>
2026-04-06 13:38:31 +0200

    made a stories slider on my posts page, fixed my make a new post modal, some changes in my profile page

---
+++ src/js/slider.js
@@ -0,0 +1,36 @@
+const slider = document.getElementById("slider");
+document.getElementById("slideLeft").addEventListener("click", () => {
+  slider.scrollBy({ left: -200, behavior: "smooth" });
+});
+document.getElementById("slideRight").addEventListener("click", () => {
+  slider.scrollBy({ left: 200, behavior: "smooth" });
+});
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("cursor-grabbing");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("cursor-grabbing");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("cursor-grabbing");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; // Scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
